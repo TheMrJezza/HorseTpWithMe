7752dc35717c3bfefa7514b823a61962c71dac6e
BuildTools <unconfigured@null.spigotmc.org>
2023-01-16 01:45:24 +1100

    Overhauled the published dev branch to match the currently used source.

--- src/main/java/com/jbouchier/horsetpwithme/HorseTpWithMe.java
+++ src/main/java/com/jbouchier/horsetpwithme/HorseTpWithMe.java
@@ -1,67 +1,35 @@
 package com.jbouchier.horsetpwithme;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Vehicle;
-import org.bukkit.event.Listener;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionDefault;
-import org.bukkit.plugin.PluginManager;
+import com.jbouchier.horsetpwithme.cmd.MainCommand;
+import com.jbouchier.horsetpwithme.cmd.Reseat;
+import com.jbouchier.horsetpwithme.cmd.TeleportAsPassenger;
+import com.jbouchier.horsetpwithme.util.DetailLogger;
+import net.minecraft.server.Main;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import static com.jbouchier.horsetpwithme.util.GeneralUtil.runTask;
+import static com.jbouchier.horsetpwithme.util.GeneralUtil.setExecutor;
 
 public class HorseTpWithMe extends JavaPlugin {
-
-    public static final boolean DETECT_NON_VANILLA_VEHICLES = checkSpigot();
-    private static final String[] WORLD_PERMS = {
-            "horsetpwithme.empty_chests_from.",
-            "horsetpwithme.empty_chests_to.",
-            "horsetpwithme.disabled_world."
-    };
-
-    private static boolean checkSpigot() {
-        try {
-            Class.forName("org.spigotmc.event.entity.EntityDismountEvent");
-            return true;
-        } catch (ClassNotFoundException ignore) {
-
-        }
-        return false;
-    }
-
     public void onEnable() {
-        MessageCache.Msg.reload();
-        PluginManager pm = getServer().getPluginManager();
-        Permission teleport = new Permission("horsetpwithme.teleport.*", PermissionDefault.TRUE);
-        pm.addPermission(teleport);
-        for (World w : Bukkit.getWorlds())
-            for (String p : WORLD_PERMS)
-                pm.addPermission(new Permission(p + w.getName().toLowerCase(), PermissionDefault.FALSE));
-        for (EntityType t : EntityType.values()) {
-            final Class<?> c = t.getEntityClass();
-            if (c != null && t.isSpawnable() && (DETECT_NON_VANILLA_VEHICLES || Vehicle.class.isAssignableFrom(c))) {
-                Permission perm = new Permission("horsetpwithme.teleport."
-                                                 + t.name().toLowerCase(), PermissionDefault.FALSE);
-                perm.addParent(teleport, true);
-                pm.addPermission(perm);
-            }
+        getServer().getPluginManager().registerEvents(TeleportListeners.INSTANCE, this);
+        runTask(PermissionHandler::build, 0);
+        DetailLogger.forceLog("§aThe plugin is enabled and working!");
+
+        setExecutor("TeleportAsPassenger", new TeleportAsPassenger());
+        setExecutor("Reseat", new Reseat());
+        setExecutor("HorseTpWithMe", new MainCommand());
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender cs, Command cmd, @NotNull String alias, String[] args) {
+        if (cmd.getName().equals("PermCheck")) {
+            if (args.length > 0) cs.sendMessage(args[0] + ": " + cs.hasPermission(args[0]));
+            return true;
         }
-
-        TeleportLogic tpLogic = new TeleportLogic(this, determineProtocol());
-        register(new CoreListeners(tpLogic));
-        if (DETECT_NON_VANILLA_VEHICLES) register(new SpigotListeners(tpLogic));
-
-        Objects.requireNonNull(getCommand("TeleportAsPassenger"),
-                "TAP Toggle Command could not be registered!").setExecutor(new TapToggleCommand(tpLogic));
-    }
-
-    private IProtocol determineProtocol() {
-        return new ProtocolHackMagic();
-    }
-
-    private void register(Listener listener) {
-        getServer().getPluginManager().registerEvents(listener, this);
+        return true;
     }
 }
---
+++ src/main/java/com/jbouchier/horsetpwithme/event/VehicleTeleportEvent.java
@@ -0,0 +1,79 @@
+package com.jbouchier.horsetpwithme.event;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class VehicleTeleportEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private boolean isCancelled;
+    private final Player driver;
+    private final Entity vehicle;
+    private final List<Entity> passengers;
+    private final Location destination, from;
+
+    /**
+     * A cancellable event that is called when a vehicle is to be teleported with its player driver.
+     * Some entities may have been filtered out of the passengers list due to permission reasons, or
+     * by choice in the case of Player passengers. tl;dr 'passengers' only contains the remaining
+     * passengers that will be teleported and does not include the driver.
+     *
+     * @param driver      The primary passenger of the vehicle at the time of teleporting
+     * @param vehicle     The vehicle entity that is teleporting
+     * @param passengers  The entities that will be teleported as passengers. Can be empty, but not null.
+     * @param destination The location where the vehicles will be sent.
+     */
+    public VehicleTeleportEvent(
+            @NotNull Player driver, @NotNull Entity vehicle,
+            @NotNull List<Entity> passengers, @NotNull Location destination
+    ) {
+        this.driver = driver;
+        this.vehicle = vehicle;
+        this.passengers = passengers;
+        this.destination = destination;
+        this.from = vehicle.getLocation();
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean isCancelled) {
+        this.isCancelled = isCancelled;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public @NotNull Location getTo() {
+        return this.destination;
+    }
+
+    public @NotNull Location getFrom() {
+        return this.from;
+    }
+
+    public @NotNull Entity getVehicle() {
+        return this.vehicle;
+    }
+
+    public @NotNull Player getDriver() {
+        return this.driver;
+    }
+
+    public @NotNull List<Entity> getPassengers() {
+        return this.passengers;
+    }
+}
